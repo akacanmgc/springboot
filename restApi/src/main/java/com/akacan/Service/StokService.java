8a74df313781953f9089827b39b8bd867b2e3eee
akacan <a.kacan@cekino.com>
2025-05-22 16:44:28 +0300

    Commit Alla

--- restApi/src/main/java/com/akacan/Service/StokService.java
+++ restApi/src/main/java/com/akacan/Service/StokService.java
@@ -1,21 +1,10 @@
 package com.akacan.Service;
 
-import com.akacan.Entities.Stok;
-import com.akacan.Repository.StokRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StokService {
 
-    private StokRepository stokRepository;
 
-    public Stok addStok(Stok stok){
-
-        Stok stokAdd = new Stok();
-        stokAdd.setName(stok.getName());
-        stokAdd.setDescription(stok.getDescription());
-        this.stokRepository.save(stokAdd);
-        return null;
-    }
 
 }
